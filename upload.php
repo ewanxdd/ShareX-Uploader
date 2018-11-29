e7ebd8f3f9a1087da4ecb65d17950a86a8cb2a2b
Ewan <vulcanv6@hotmail.com>
2018-11-29 14:30:21 +0000

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,53 @@
+<?php
+$tokens = array("setatokenhere"); //Stick token here
+ 
+$sharexdir = "images/"; 
+$lengthofstring = 5; 
+//Random file name generation
+function RandomString($length) {
+    $keys = array_merge(range(0,9), range('a', 'z'));
+ 
+    for($i=0; $i < $length; $i++) {
+        $key .= $keys[mt_rand(0, count($keys) - 1)];
+    }
+    return $key;
+}
+
+if(isset($_POST['secret']))
+{
+   
+    if(in_array($_POST['secret'], $tokens))
+    {
+        //Prepares for upload
+        $filename = RandomString($lengthofstring);
+        $target_file = $_FILES["sharex"]["name"];
+        $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+        
+        //Accepts and moves to directory
+        if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$filename.'.'.$fileType))
+        {
+            //Sends info to client
+            $json->status = "OK";
+            $json->errormsg = "";
+            $json->url = $filename . '.' . $fileType;
+        }
+            else
+        {
+            
+           echo 'File upload failed - CHMOD/Folder doesn\'t exist?';
+        }  
+    }
+    else
+    {
+        //Invalid key
+        echo 'Invalid Secret Key';
+    }
+}
+else
+{
+    
+    echo 'No post data recieved';
+}
+
+echo(json_encode($json));
+?>
